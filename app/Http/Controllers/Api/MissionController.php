dc2a10277d26d6773d7997445ff295c6a1172b87
David <david_ballesteros@data4you.cz>
2024-11-13 09:53:15 +0100

    fixed mission controller

--- app/Http/Controllers/Api/MissionController.php
+++ app/Http/Controllers/Api/MissionController.php
@@ -5,13 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Mission;
-use App\Models\Person;
-use Illuminate\Validation\Rule;
-use Illuminate\Support\Facades\Mail;
-use App\Mail\SendMissionDetails;
-use Illuminate\Support\Facades\Notification;
-use App\Notifications\MissionOutcomeUpdated;
-use App\Models\User;
 
 class MissionController extends Controller
 {
@@ -41,9 +34,8 @@ class MissionController extends Controller
 
         if ($mission_id) {
             $mission = Mission::findOrFail($mission_id);
-            $old_mission_outcome = $mission->outcome;
         } else {
-            $mission = new Mission;
+            $mission = new Mission();
         }
 
         $mission->name = $request->input('name');
@@ -51,110 +43,9 @@ class MissionController extends Controller
         $mission->outcome = $request->input('outcome');
         $mission->save();
 
-        // if outcome has changed
-        if ($mission_id && ($old_mission_outcome != $mission->outcome)) {
-            // $user->notify(new Notification($mission))
-            $admins = User::where('role', 'admin')->get();
-            Notification::send($admins, new MissionOutcomeUpdated($mission));
-        }
-
         return [
             'status' => 'success',
             'message' => 'The mission has been updated'
-        ];
-    }
-
-    public function assignPerson(Request $request)
-    {
-        $mission_id = $request->input('mission_id');
-        $person_id = $request->input('person_id');
-
-        $request->validate([
-            'mission_id' => Rule::unique('mission_person')->where(function ($query) use($mission_id,$person_id) {
-                return $query->where('mission_id', $mission_id)
-                    ->where('person_id', $person_id);
-            }),
-        ], [
-            'mission_id.unique' => "The person has been already assigned to the mission"
-        ]);
-
-        $mission = Mission::find($mission_id);
-
-        if (!$mission) {
-            return [
-                'status' => 'fail',
-                'message' => 'Mission with the id ' . $mission_id . ' does not exist'
-            ];
-        }
-
-        $person = Person::find($person_id);
-
-        if (!$person) {
-            return [
-                'status' => 'fail',
-                'message' => 'Person with the id ' . $person_id . ' does not exist'
-            ];
-        }
-
-        $mission->people()->attach($person->id);
-
-        return [
-            'status' => 'success',
-            'message' => 'Person attached to the mission successfully'
-        ];
-    }
-
-    public function unassignPerson(Request $request)
-    {
-        $mission_id = $request->input('mission_id');
-        $person_id = $request->input('person_id');
-
-        $mission = Mission::find($mission_id);
-
-        if (!$mission) {
-            return [
-                'status' => 'fail',
-                'message' => 'Mission with the id ' . $mission_id . ' does not exist'
-            ];
-        }
-
-        $person = Person::find($person_id);
-
-        if (!$person) {
-            return [
-                'status' => 'fail',
-                'message' => 'Person with the id ' . $person_id . ' does not exist'
-            ];
-        }
-
-        $mission->people()->detach($person->id);
-
-        return [
-            'status' => 'success',
-            'message' => 'Person detached from the mission successfully'
-        ];
-    }
-
-    public function sendMissionDetails(Request $request)
-    {
-        $mission_id = $request->input('mission_id');
-        $user = auth()->user();
-
-        $mission = Mission::find($mission_id);
-
-        if (!$mission) {
-            return [
-                'status' => 'fail',
-                'message' => 'Mission with the id ' . $mission_id . ' does not exist'
-            ];
-        }
-
-        Mail::to($user->email)
-            ->send(new SendMissionDetails($mission));
-
-        return [
-            'status' => 'success',
-            'message' => 'Email was sent successfully'
         ];
     }
 }
